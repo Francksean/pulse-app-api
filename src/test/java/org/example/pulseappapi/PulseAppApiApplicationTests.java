ce041446213435cec0675264e81a17ec0feab558
franck sean <seandjissou@gmail.com>
2025-01-27 11:27:23 +0100

    add project to git

---
+++ src/test/java/org/example/pulseappapi/PulseAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pulseappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
